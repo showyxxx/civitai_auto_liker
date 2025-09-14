b9841c319578352b073f49f1e56d3c418476b1f9
showyxx <vadim4ikpupozzi@gmail.com>
2025-09-14 13:42:15 +0300

    Update config.py

--- config.py
+++ config.py
@@ -12,3 +12,16 @@ HEADLESS_MODE = False  # True - stealth mode, False - idk... mb normal mode
 SESSION_SAVE_DELAY = 5  # maybe session settings
 ACTION_DELAY = 1  # delay
 AUTO_WAIT_FOR_USER = True  # wait for user response (recommended = True)
+
+
+# script settings
+AUTO_UPDATE = False  # default; True = update automatically without confirmation
+UPDATE_BRANCH = "main"  # higly NOT recommended to change this option; 2 available branch: main and indev (I will not be responsible for anything breaking for you if you change the value of this option)
+
+
+
+
+
+
+
+CURRENT_VERSION = "v1.0.0"
